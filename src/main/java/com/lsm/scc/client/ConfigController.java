0a60e4fa3c16a0f7827d644a5f4760221af751d3
17727921123 <caiang.666>
2020-08-09 16:05:06 +0800

    SpringCloudBus

--- src/main/java/com/lsm/scc/client/ConfigController.java
+++ src/main/java/com/lsm/scc/client/ConfigController.java
@@ -1,17 +1,25 @@
 package com.lsm.scc.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RefreshScope注解加在需要动态读取配置的类或方法上
+@RefreshScope
 public class ConfigController {
 
-    @Autowired
-    private Config config;
+    @Value("${data.env}")
+    private String env;
+    @Value("${data.user.username}")
+    private String username;
+    @Value("${data.user.password}")
+    private String password;
 
     @GetMapping(value = "getConfig")
-    public Config getConfig() {
-        return config;
+    public String getConfig() {
+        return env + "," + username + "," + password;
     }
+
 }
